6b0559a1b5e9822b95ffd851754972cb7b228a64
Pham Nguyen Trinh <hlv.nguyentrinh@gmail.com>
2018-06-02 00:59:39 +0700

    Reduce output for easier debugging

--- build_mir3g.sh
+++ build_mir3g.sh
@@ -10,7 +10,7 @@ RELEASE_MODULES="luci minidlna luci-app-minidlna \
 echo "Begin build ${RELEASE_NAME} with modules ${RELEASE_MODULES}"
 	
 wget https://downloads.openwrt.org/snapshots/targets/ramips/mt7621/openwrt-imagebuilder-ramips-mt7621.Linux-x86_64.tar.xz
-tar -xvf openwrt-imagebuilder-ramips-mt7621.Linux-x86_64.tar.xz
+tar -xvf openwrt-imagebuilder-ramips-mt7621.Linux-x86_64.tar.xz >/dev/null
 rm -f openwrt-imagebuilder-ramips-mt7621.Linux-x86_64.tar.xz
 cd openwrt-imagebuilder-ramips-mt7621.Linux-x86_64
 make image PROFILE=mir3g "PACKAGES=${RELEASE_MODULES}"
