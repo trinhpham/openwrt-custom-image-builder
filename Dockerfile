76c8ae3266c1361efc23016b6a8c6538109d39ff
Trinh Pham <trinh.pham@logigear.com>
2018-06-01 17:44:00 +0700

    As Travis-CI doesn't support docker volume mounting => make all tasks go inside docker container

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,17 @@
+FROM golang
+
+RUN go get github.com/aktau/github-release
+
 FROM centos
 
-RUN yum install -y subversion git gawk gettext time which \
-		ncurses-devel zlib-devel openssl-devel libxslt wget && \
+RUN yum install -y yum-plugin-ovl subversion git gawk gettext time \
+		which ncurses-devel zlib-devel openssl-devel libxslt wget && \
 	yum group install -y "Development Tools" && \
 	yum clean all
 
-RUN wget https://downloads.openwrt.org/snapshots/targets/ramips/mt7621/openwrt-imagebuilder-ramips-mt7621.Linux-x86_64.tar.xz \
-	&& tar -xvf openwrt-imagebuilder-ramips-mt7621.Linux-x86_64.tar.xz \
-	&& rm -f openwrt-imagebuilder-ramips-mt7621.Linux-x86_64.tar.xz
+COPY --from=0 /go/bin/github-release /usr/local/bin/
+ADD build_mir3g.sh /src/
+	
+WORKDIR /src
 
-WORKDIR /openwrt-imagebuilder-ramips-mt7621.Linux-x86_64
-
-#RUN make image PROFILE=mir3g PACKAGES="luci minidlna luci-app-minidlna samba36-server transmission-web openvpn-openssl openssl-util luci-app-openvpn kmod-usb-storage kmod-fs-ext4 kmod-fs-vfat kmod-nls-cp437 kmod-nls-iso8859-1 kmod-fs-msdos kmod-fs-ntfs block-mount"
+CMD ["/src/build_mir3g.sh"]
---
+++ build_mir3g.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+wget https://downloads.openwrt.org/snapshots/targets/ramips/mt7621/openwrt-imagebuilder-ramips-mt7621.Linux-x86_64.tar.xz
+tar -xvf openwrt-imagebuilder-ramips-mt7621.Linux-x86_64.tar.xz
+rm -f openwrt-imagebuilder-ramips-mt7621.Linux-x86_64.tar.xz
+cd openwrt-imagebuilder-ramips-mt7621.Linux-x86_64
+make image PROFILE=mir3g PACKAGES="luci minidlna luci-app-minidlna \
+	samba36-server transmission-web openvpn-openssl openssl-util \
+	luci-app-openvpn kmod-usb-storage kmod-fs-ext4 kmod-fs-vfat \
+	kmod-nls-cp437 kmod-nls-iso8859-1 kmod-fs-msdos kmod-fs-ntfs \
+	block-mount"
+
+RELEASE_NAME="v0.1-$(date +%Y%m%d_%H%M%S)"
+	
+github-release release \
+    --user trinhpham \
+    --repo xiaomi-r3g-openwrt-builder \
+    --tag $RELEASE_NAME \
+    --name $RELEASE_NAME \
+    --description "Daily build"
+	
+github-release upload \
+    --user trinhpham \
+    --repo xiaomi-r3g-openwrt-builder \
+    --tag $RELEASE_NAME \
+    --name openwrt-ramips-mt7621-device-mir3g.manifest \
+	--file /src/openwrt-imagebuilder-ramips-mt7621.Linux-x86_64/bin/targets/ramips/mt7621/openwrt-ramips-mt7621-device-mir3g.manifest
+	
+github-release upload \
+    --user trinhpham \
+    --repo xiaomi-r3g-openwrt-builder \
+    --tag $RELEASE_NAME \
+    --name openwrt-ramips-mt7621-mir3g-squashfs-rootfs0.bin \
+	--file /src/openwrt-imagebuilder-ramips-mt7621.Linux-x86_64/bin/targets/ramips/mt7621/openwrt-ramips-mt7621-mir3g-squashfs-rootfs0.bin
+	
+github-release upload \
+    --user trinhpham \
+    --repo xiaomi-r3g-openwrt-builder \
+    --tag $RELEASE_NAME \
+    --name sha256sums \
+	--file /src/openwrt-imagebuilder-ramips-mt7621.Linux-x86_64/bin/targets/ramips/mt7621/sha256sums
+
+github-release upload \
+    --user trinhpham \
+    --repo xiaomi-r3g-openwrt-builder \
+    --tag $RELEASE_NAME \
+    --name openwrt-ramips-mt7621-mir3g-squashfs-kernel1.bin \
+	--file /src/openwrt-imagebuilder-ramips-mt7621.Linux-x86_64/bin/targets/ramips/mt7621/openwrt-ramips-mt7621-mir3g-squashfs-kernel1.bin
+	
+github-release upload \
+    --user trinhpham \
+    --repo xiaomi-r3g-openwrt-builder \
+    --tag $RELEASE_NAME \
+    --name openwrt-ramips-mt7621-mir3g-squashfs-sysupgrade.tar \
+	--file /src/openwrt-imagebuilder-ramips-mt7621.Linux-x86_64/bin/targets/ramips/mt7621/openwrt-ramips-mt7621-mir3g-squashfs-sysupgrade.tar
