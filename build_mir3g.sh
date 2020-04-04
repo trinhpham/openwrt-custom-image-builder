624582a297103bc98388efd1cba496e3cda7a713
marcelomartinsIT <57966284+marcelomartinsIT@users.noreply.github.com>
2020-04-04 15:54:57 +0100

    Phyton Build Correction
    Phyton build correction on the build_mir3g.sh

--- build_mir3g.sh
+++ build_mir3g.sh
@@ -9,7 +9,8 @@ GIT_REPO_NAME=${GIT_REPO##*/}
 
 echo "Begin build ${RELEASE_NAME} with modules ${RELEASE_MODULES}"
 
-scl enable rh-python36 bash
+# scl enable rh-python36 bash
+source /opt/rh/rh-python36/enable
 
 wget https://downloads.openwrt.org/snapshots/targets/ramips/mt7621/openwrt-imagebuilder-ramips-mt7621.Linux-x86_64.tar.xz
 tar -xvf openwrt-imagebuilder-ramips-mt7621.Linux-x86_64.tar.xz >/dev/null
