7dfa80bb76cfae358177656867194329820c95a1
Trinh Pham <9128061+trinhpham@users.noreply.github.com>
2020-07-03 15:29:47 +0700

    Update Python 3.8

--- build_mir3g.sh
+++ build_mir3g.sh
@@ -9,8 +9,8 @@ GIT_REPO_NAME=${GIT_REPO##*/}
 
 echo "Begin build ${RELEASE_NAME} with modules ${RELEASE_MODULES}"
 
-# scl enable rh-python36 bash
-source /opt/rh/rh-python36/enable
+# scl enable rh-python38 bash
+source /opt/rh/rh-python38/enable
 
 wget https://downloads.openwrt.org/snapshots/targets/ramips/mt7621/openwrt-imagebuilder-ramips-mt7621.Linux-x86_64.tar.xz
 tar -xvf openwrt-imagebuilder-ramips-mt7621.Linux-x86_64.tar.xz >/dev/null
