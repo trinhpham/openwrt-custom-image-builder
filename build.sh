3f616aaf0dff96d196df7842bb7669fd1bdf1d99
Trinh Pham <trinh.pham@linecorp.com>
2022-04-10 15:27:22 +0700

    Update project to CircleCI

--- build.sh
+++ build.sh
@@ -9,4 +9,6 @@ ADDITIONAL_PACKAGES=`cat custom_scripts/profiles/$1/modules.txt`
 
 export RELEASE_PACKAGES=`echo -e $ADDITIONAL_PACKAGES\n$BUILTIN_PACKAGES | sort | uniq | tr "\n" " "`
 
+echo "Start building with these packages: $RELEASE_PACKAGES"
+
 make image PROFILE=$1 PACKAGES=$RELEASE_PACKAGES
