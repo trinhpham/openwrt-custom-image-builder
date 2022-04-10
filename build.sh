1b13b07d00de650e88e7313363c5a5dc3a25dd5d
Trinh Pham <9128061+trinhpham@users.noreply.github.com>
2022-04-10 07:31:26 +0000

    Correct version finder

--- build.sh
+++ build.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-cd ..
 pwd
 
 BUILTIN_PACKAGES=`make info | grep -A2 "^${1}:$" | grep -oP "Packages:\K.*" | tr " " "\n"`
 echo "Builtin packages: $BUILTIN_PACKAGES"
 
-ADDITIONAL_PACKAGES=`cat profiles/$1/modules.txt`
+ADDITIONAL_PACKAGES=`cat custom_scripts/profiles/$1/modules.txt`
 
 export RELEASE_PACKAGES=`echo -e $ADDITIONAL_PACKAGES\n$BUILTIN_PACKAGES | sort | uniq | tr "\n" " "`
 
