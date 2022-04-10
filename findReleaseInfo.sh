8616af83b9b65a012d5a8c1945b34083f466c3e3
Trinh Pham <9128061+trinhpham@users.noreply.github.com>
2022-04-10 07:25:25 +0000

    Correct version finder

--- findReleaseInfo.sh
+++ findReleaseInfo.sh
@@ -3,11 +3,12 @@
 export RELEASE_ARCH_SOC=`cat profiles/$1/arch_soc.txt`
 
 if [ "$2" == "stable" ]; then 
-	export RELEASE_VER=`curl -s https://openwrt.org/ | grep -oP -m1 'Current Stable Release[^0-9]*\K[0-9]*\.[0-9]*\.[0-9]*'`
+	RELEASE_VER=`curl -s https://openwrt.org/ | grep -oP -m1 'Current Stable Release[^0-9]*\K[0-9]*\.[0-9]*'`
 	if [ -z "$RELEASE_VER" ]; then
 		echo "Unable to find the stable version of OpenWrt"
 		exit 2
 	fi
+	export RELEASE_VER="openwrt-$RELEASE_VER"
 else
 	export RELEASE_VER=$2
 fi
