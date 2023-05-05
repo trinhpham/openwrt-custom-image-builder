c6cbcc273bc9a881a30ec5b5b95c058ebc36ddcd
Trinh Pham <9128061+trinhpham@users.noreply.github.com>
2023-05-05 08:48:28 +0000

    Update new image tag format of image builder

--- findReleaseInfo.sh
+++ findReleaseInfo.sh
@@ -3,14 +3,17 @@
 export RELEASE_ARCH_SOC=`cat profiles/$1/arch_soc.txt`
 
 if [ "$2" == "stable" ]; then 
-	RELEASE_VER=`curl -s https://openwrt.org/ | grep -oP 'Current Stable Release[^0-9]*\K[0-9]*\.[0-9]*' | head -1`
+	RELEASE_VER=`curl -s https://openwrt.org/ | grep -oP 'Current Stable Release[^0-9]*\K[0-9]*\.[0-9]*.[0-9]*' | head -1`
 	if [ -z "$RELEASE_VER" ]; then
 		echo "Unable to find the stable version of OpenWrt"
 		exit 2
 	fi
-	export RELEASE_VER="openwrt-$RELEASE_VER"
+	export RELEASE_VER="v$RELEASE_VER"
 else
 	export RELEASE_VER=$2
 fi
 
 export SOURCE_DATE_EPOCH="$(git log -1 --format=%at $(git describe --abbrev=0))"
+
+echo "Release version: $RELEASE_VER"
+echo "Build script date: $SOURCE_DATE_EPOCH"
